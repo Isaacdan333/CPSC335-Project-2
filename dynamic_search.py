d639211f32352c789f2b3a9e2d9d6401fd78666d
Isaac Daniel Perez <isaacdan333@gmail.com>
2023-12-01 20:26:55 -0800

    Project 2

--- dynamic_search.py
+++ dynamic_search.py
@@ -2,23 +2,29 @@ def max_stocks(N, Stocks_and_values, Amount):
     # 2D array
     possible_stock = [[0] * (Amount + 1) for a in range(N + 1)]
 
-    for i in range(1, N + 1):
-        for j in range(Amount + 1):
+    for i in range(1, N + 1): #2
+        for j in range(Amount + 1): #5
             # if the current stock value is greater than the available amount, skip it
             if Stocks_and_values[i - 1][1] > j:
                 possible_stock[i][j] = possible_stock[i - 1][j]
-                #print(dp)
+                #print(possible_stock)
             else:
                 # Consider the maximum value of either including or excluding the current stock
                 possible_stock[i][j] = max(possible_stock[i - 1][j], possible_stock[i - 1][j - Stocks_and_values[i - 1][1]] + Stocks_and_values[i - 1][0])
-                #print(dp[i][j])
+                #print(possible_stock)
+        #print(possible_stock)
 
     return possible_stock[N][Amount]
 
-N = 4
+N = 0
 Stocks_and_values = [ [3, 2], [4, 3], [5, 3], [6, 7] ]
 Amount = 10
 
-result = max_stocks(N, Stocks_and_values, Amount)
-#print(a)
-print(result)
+for i in Stocks_and_values:
+    N += 1
+
+if N > 0 and N < 1000:
+    result = max_stocks(N, Stocks_and_values, Amount)
+    print("The maximum amount for stocks to buy are:", result)
+else:
+    print("Invalid constraints")
---
+++ exhaustive_search.py
@@ -0,0 +1,35 @@
+def stock_maximization(N, Stocks_and_values, Amount):
+    max_stocks = 0
+    max_stocks_combination = None
+
+    # make all possible combinations
+    for i in range(2**N):
+        current_stock = 0
+        current_value = 0
+        combinations = []
+
+        for j in range(N):
+            if (i >> j) & 1:
+                current_stock += Stocks_and_values[j][0]
+                current_value += Stocks_and_values[j][1]
+                combinations.append(j)
+
+        #check to see if combination valid and maximizes # of stocks
+        if current_value <= Amount and current_stock > max_stocks:
+            max_stocks = current_stock
+            #max_stocks_combination = combinations
+
+    return max_stocks
+
+N = 0
+Stocks_and_values = [[1, 2], [4, 3], [5, 6], [6, 7]]
+Amount = 12
+
+for i in Stocks_and_values:
+    N += 1
+
+if N > 0 and N < 1000:
+    result = stock_maximization(N, Stocks_and_values, Amount)
+    print("Maximum number of stocks:", result)
+else:
+    print("Invalid constraints")
